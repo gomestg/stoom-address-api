de6993fc13334e78b473f53475409cf550dc6455
Gomes, Tiago <gomes.tg@hotmail.com>
2021-04-29 01:10:00 -0300

    Initialize main

---
+++ address/src/main/java/com/stoom/address/config/SwaggerConfig.java
@@ -0,0 +1,36 @@
+package com.stoom.address.config;
+
+import com.google.common.base.Predicates;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Value("${swagger.config.host:}")
+    private String host = new String();
+
+    @Bean
+    public Docket api() {
+        Docket docket = new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .paths(Predicates.not(PathSelectors.regex("/error")))
+                .build();
+
+        if (!host.isEmpty()) {
+            docket.host(host);
+        }
+
+        return docket;
+    }
+
+}
